3523ebf422697375e101c0fce68a1f5353e655c5
Osbert Ngok <osbertngok@gmail.com>
2023-12-09 17:49:00 +0800

    Initial Commit

---
+++ AWS IP Switcher/AWS_IP_SwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWS_IP_SwitcherApp.swift
+//  AWS IP Switcher
+//
+//  Created by Osbert Nephide Ngok on 9/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AWS_IP_SwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AWS IP Switcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AWS IP Switcher
+//
+//  Created by Osbert Nephide Ngok on 9/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
